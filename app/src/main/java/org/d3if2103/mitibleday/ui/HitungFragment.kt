0f0686241bfc2c434f2542b41f500f7cee64bdfa
MuhammadLutfi <mhdlutfi@student.telkomuniversity.ac.id>
2022-05-24 15:40:41 +0700

    Membuat navigasi sederhana & Mengirim data saat bernavigasi & Membuat navigasi menggunakan menu & Membuat navigasi ke app lain

--- app/src/main/java/org/d3if2103/mitibleday/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if2103/mitibleday/ui/HitungFragment.kt
@@ -1,14 +1,14 @@
 package org.d3if2103.mitibleday.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if2103.mitibleday.R
 import org.d3if2103.mitibleday.databinding.FragmentHitungBinding
 import org.d3if2103.mitibleday.model.harga
@@ -18,9 +18,33 @@ class HitungFragment : Fragment() {
 
     private lateinit var binding: FragmentHitungBinding
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.tentang_aplikasi_zakat, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this)[MainViewModel::class.java]
+    }
+    private fun shareData(){
+        val message = getString(R.string.Bagikan_template,binding.hargaTextView.text)
+
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, message)
+        if (shareIntent.resolveActivity(
+                requireActivity().packageManager) !=null) {
+            startActivity(shareIntent)
+        }
     }
 
 
@@ -28,12 +52,14 @@ class HitungFragment : Fragment() {
 
 
         binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
 
         binding.btnHitung.setOnClickListener { hitungBmi() }
-        binding.tuntunanButton.setOnClickListener(){
+        binding.bagikanButton.setOnClickListener { shareData() }
+        binding.tuntunanButton.setOnClickListener {
             it.findNavController().navigate(
                 R.id.action_hitungFragment_to_tuntunanFragment
             )
@@ -72,7 +98,7 @@ class HitungFragment : Fragment() {
     private fun showResult(result: harga?){
         if (result == null) return
         binding.hargaTextView.text = getString(R.string.harga,result.harga)
-        binding.tuntunanButton.visibility = View.VISIBLE
+        binding.bagikanButton.visibility = View.VISIBLE
     }
 
 }
